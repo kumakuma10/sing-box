92db552e169215cccd7a0aa748f76be50fbd8845
kumakuma10 <pbhoxh7e@duck.com>
2023-09-27 19:47:21 +0800

    fix: hy2

---
+++ inbound/hysteria2_stub.go
@@ -0,0 +1,32 @@
+//go:build !with_quic
+
+package inbound
+
+import (
+	"context"
+
+	"github.com/kumakuma10/sing-box/adapter"
+	C "github.com/kumakuma10/sing-box/constant"
+	"github.com/kumakuma10/sing-box/log"
+	"github.com/kumakuma10/sing-box/option"
+)
+
+type Hysteria2 struct {
+	adapter.Inbound
+}
+
+func (h *Hysteria2) Start() error {
+	return C.ErrQUICNotIncluded
+}
+
+func (h *Hysteria2) AddUsers(_ []option.Hysteria2User) error {
+	return C.ErrQUICNotIncluded
+}
+
+func (h *Hysteria2) DelUsers(_ []string) error {
+	return C.ErrQUICNotIncluded
+}
+
+func NewHysteria2(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.Hysteria2InboundOptions) (adapter.Inbound, error) {
+	return nil, C.ErrQUICNotIncluded
+}
